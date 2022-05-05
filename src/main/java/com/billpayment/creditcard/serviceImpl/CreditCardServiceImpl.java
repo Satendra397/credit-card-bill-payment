370f5d6e3b8b150c9340ed124ae28f0f6e4ff1b0
satendra pal <satendrapal9111@gmail.com>
2022-05-05 21:32:07 +0530

    created register endpoint and save the detail in the database we use post mapping for login

--- src/main/java/com/billpayment/creditcard/serviceImpl/CreditCardServiceImpl.java
+++ src/main/java/com/billpayment/creditcard/serviceImpl/CreditCardServiceImpl.java
@@ -2,7 +2,9 @@ package com.billpayment.creditcard.serviceImpl;
 
 import com.billpayment.creditcard.dto.BaseResponse;
 import com.billpayment.creditcard.dto.RegisterDetailRequest;
+import com.billpayment.creditcard.dto.UserDetailRequest;
 import com.billpayment.creditcard.entity.Register;
+import com.billpayment.creditcard.entity.User;
 import com.billpayment.creditcard.repository.CreditCardDAO;
 import com.billpayment.creditcard.repository.RegisterDAO;
 import com.billpayment.creditcard.repository.UserDAO;
@@ -13,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CreditCardServiceImpl implements CreditCardService {
@@ -27,20 +30,22 @@ public class CreditCardServiceImpl implements CreditCardService {
     @Autowired
     private RegisterDAO registerDAO;
 
-
+/*
+* post mapping for register user detail
+*
+* */
 
     @Override
-    public ResponseEntity<BaseResponse> registerUserDetail(RegisterDetailRequest registerDetailRequest) {
+    public ResponseEntity<BaseResponse> userDetailRequest(UserDetailRequest userDetailRequest) {
 
-        Register register = new Register();
+        User user = new User();
 
-        register.setRegisterId(registerDetailRequest.getRegisterId());
-        register.setRegisterName(registerDetailRequest.getRegisterName());
-        register.setRegisterMobileNo(registerDetailRequest.getRegisterMobileNo());
-        register.setRegisterEmail(registerDetailRequest.getRegisterEmail());
-        register.setRegisterPassword(registerDetailRequest.getRegisterPassword());
-
-        registerDAO.save(register);
+        user.setUserId(userDetailRequest.getUserId());
+        user.setUserName(userDetailRequest.getUserName());
+        user.setMobileNo(userDetailRequest.getMobileNo());
+        user.setUserEmail(userDetailRequest.getUserEmail());
+        user.setUserPassword(userDetailRequest.getUserPassword());
+        userDAO.save(user);
 
         BaseResponse baseResponse = new BaseResponse();
         baseResponse.setMessage("detail save successfully!!!");
@@ -50,8 +55,59 @@ public class CreditCardServiceImpl implements CreditCardService {
 
     }
 
+    /*
+    * post mapping for login
+    * */
     @Override
-    public ResponseEntity<BaseResponse> loginUser(int registerId, String registerPassword, RegisterDetailRequest registerDetailRequest) {
-        return null;
+    public ResponseEntity<BaseResponse> loginUser( UserDetailRequest userDetailRequest) {
+
+
+
+        Optional<Object> user= Optional.ofNullable( userDAO.login(userDetailRequest.getUserEmail(),userDetailRequest.getUserPassword()));
+
+        BaseResponse baseResponse = new BaseResponse();
+        if (!user.isPresent()) {
+            baseResponse.setMessage("user not found ");
+            baseResponse.setHttpStatus(HttpStatus.NOT_FOUND);
+            baseResponse.setHttpStatusCode(HttpStatus.NOT_FOUND.value());
+
+            return new ResponseEntity<>(baseResponse,HttpStatus.NOT_FOUND);
+        }
+        baseResponse.setMessage("user login successfully");
+        baseResponse.setHttpStatus(HttpStatus.OK);
+        baseResponse.setHttpStatusCode(HttpStatus.OK.value());
+
+        return new ResponseEntity<>(baseResponse,HttpStatus.OK);
+/*
+        User user = new User();
+        user.setUserId(userDetailRequest.getUserId());
+        user.setUserName(userDetailRequest.getUserName());
+        user.setUserEmail(userDetailRequest.getUserEmail());
+        user.setUserPassword(userDetailRequest.getUserPassword());
+        user.setMobileNo(userDetailRequest.getMobileNo());
+        userDAO.save(user);*/
+       /* BaseResponse baseResponse1=new BaseResponse();
+        baseResponse1.setMessage("user data save successfully");
+        baseResponse1.setHttpStatus(HttpStatus.OK);
+        baseResponse1.setHttpStatusCode(HttpStatus.OK.value());
+
+        return new ResponseEntity<>(baseResponse1,HttpStatus.OK);*/
+
+
+
+     /*   if (registerEmail == userEmail && registerPassword == userPassword)
+        {
+            BaseResponse baseResponse = new BaseResponse();
+            baseResponse.setMessage("user login successfully");
+            baseResponse.setHttpStatus(HttpStatus.OK);
+            baseResponse.setHttpStatusCode(HttpStatus.OK.value());
+
+            return new ResponseEntity<>(baseResponse,HttpStatus.OK);
+        }
+*/
+
+
+
+
     }
 }
